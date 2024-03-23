34fc080d396ca3d5f6ae2b736a0dce4d5ef34325
PinJhih <isaacwang29@gmail.com>
2024-03-23 11:07:05 +0800

    List activities hosted by user

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const users = require("../models/users");
+const activities = require("../models/activities");
 
 router.get("/", async (req, res) => {
   const userID = req.locals.userID;
   const username = await users.getName(userID);
+  const activityList = await activities.getActivities(userID);
+  
   res.locals.username = username;
-  res.render("index", { title: "Welcome to my Project!" });
+  res.render("index", {
+    title: "Welcome to my Project!",
+    activities: activityList,
+  });
 });
 
 module.exports = router;
